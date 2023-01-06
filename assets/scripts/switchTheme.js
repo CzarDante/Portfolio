36b28a1b8e7648b3f2877629ada5befc0082fb82
CzarDante <duducezarfernandes@gmail.com>
2023-01-06 17:08:30 -0300

    theme-toggle upgrade

--- assets/scripts/switchTheme.js
+++ assets/scripts/switchTheme.js
@@ -1,4 +1,6 @@
 var toggle = document.getElementById("theme-toggle");
+var circle_toggle = document.getElementById("circle-toggle");
+
 
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 if (storedTheme)
@@ -8,9 +10,11 @@ if (storedTheme)
 toggle.onclick = function() {
     var currentTheme = document.documentElement.getAttribute("data-theme");
     var targetTheme = "light";
+    circle_toggle.style.transform="translateX(0px)";
 
     if (currentTheme === "light") {
         targetTheme = "dark";
+        circle_toggle.style.transform="translateX(38px)";
     }
 
     document.documentElement.setAttribute('data-theme', targetTheme)
